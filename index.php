3a38694b8163ec8d7a23ecc849e408cc38509b24
davidjwz <david.meulenbeld@gmail.com>
2014-03-23 17:40:22 +0100

    Eerste commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="nl">
+	<head>
+		<title>The Locals</title>
+		<link rel="stylesheet" type="text/css" href="css/reset.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	
+	<body>
+		<div id="Main-Wrapper">
+
+			<div id="Fotos">
+				<a href="index.php"><p id="logo">Bobbie & Zora (4+)</p></a>
+				<a href="https://www.facebook.com/"><img src="images/facebook.png" id="facebook"></a>
+				<a href="https://www.youtube.com/"><img src="images/youtube.png" id="youtube"></a>
+
+			</div>
+									<div id="Nav-Bar">
+				<li><a href="" id="normal">Geweest</a></li>
+				<li><a href="" id="active">Nu</a></li>
+				<li><a href="" id="normal">Verwacht</a></li>
+				<li><a href="" id="normal">Over ons</a></li>
+				<li><a href="" id="normal">Speellijst</a></li>
+				<li><a href="" id="normal">Boekingen</a></li>
+			</div>
+
+
+			<div id="nu"><img src="images/flyer.jpg" id="flyer">
+				<div id="tekst1"><p id="kop1">Bobbie &#38 Zora (4+)</p><br>
+<p id="schuin">Een humoristische, muzikale en kleurrijke voorstelling over het overwinnen van de angst voor het onbekende.</p><br>
+Daar woont Bobbie. Zijn huis is helemaal wit. Bobbie houdt van melk, van rust en van opgeruimd. Op een dag verschijnt er vanuit het niets een confettisnipper. Langzaam dringen ze &#233;&#233;n voor &#233;&#233;n zijn witte wereld binnen. Het is de kleurrijke Zora. Ze is gevlucht uit het circus. Zora houdt van kleur en van verrassingen maar Bobbie niet. Het is niet echt vriendschap op het eerste gezicht wanneer Bobbie & Zora elkaar ontmoeten. Toch worden ze uiteindelijk vrienden, goede vrienden zelfs. Als Zora op een dag ontvoerd wordt door haar oude circusdirecteur Mortelli, twijfelt Bobbie niet lang. Een spannende zoektocht naar Zora begint. Zou zijn reddingsactie lukken, of verdwaalt hij in de grote wijde wereld&#63;
+<br><br>Concept & Spel: Denise Lukkenaer & Ferry Spigt
+Coaching & Eindregie: Ramses Graus (Het Filiaal)
+Tekst: Anouk Saleming
+Vormgeving: Jasper van Roden, Thiadmer van Galen
+Muziek: Denise Lukkenaer, Henk Meulenbeld
+Fotografie/flyerontwerp: Kris van Veen
+Zakelijk Leiding: Hilje Thiescheffer
+Met dank aan: Manon van den Berg, Rogier Brom, Willem Meulenbeld, Ralf Pouw en Job Taks.
+ Bobbie & Zora is te boeken via impresariaat Buro bannink: www.burobannink.nl/
+
+
+
+</div>
+
+
+
+			</div>
+<hr>
+
+
+		</div>
+
+	</body>	
+</html>	
